fb0d4f9197f6036129ce0046f7f0c32bf79ec910
Chizzo Cheese <chizzo84@gmail.com>
2020-04-10 14:18:36 +0200

    Update CovidCaseController.php

--- app/Http/Controllers/CovidCaseController.php
+++ app/Http/Controllers/CovidCaseController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 class CovidCaseController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
